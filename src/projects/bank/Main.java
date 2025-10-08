66eade02a7d6aa963461e8b4110e9d144a93de45
John M Dusel <jdusel@frederick.edu>
2025-10-08 17:13:36 +0000

    ENH: projects.bank: Add driver code
    Dusel will maintain this file as we finish future phases.
    
    - Uncomment lines 36, 42 after fixing the signature of `Bank.find`.
    
    - Delete lines 35, 37, 41, 43 after fixing the signature of `Bank.find`.

---
+++ src/projects/bank/Main.java
@@ -0,0 +1,66 @@
+package projects.bank;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        phase1();
+    }
+
+    public static void phase1() {
+        String logName = "phase1.log";
+        try {
+            FileWriter writer = new FileWriter(new File(logName));
+
+            Account acct = new Account(
+                "id1",
+                "Owner Name",
+                AccountType.SAVINGS,
+                1.0
+            );
+            
+            writer.write(
+                String.format(
+                    "Account setup: acct id=%s, owner=%s, balance=%f",
+                    acct.getAccountID(),
+                    acct.getOwnerName(),
+                    acct.getBalance()
+                ) + System.lineSeparator()
+            );
+
+            Bank bank = new Bank();
+            int numAccounts0 = bank.totalAccounts();
+            // TODO run after fixing signature
+            // int findAcct0 = bank.find(acct.getAccountID());
+            int findAcct0 = -1;
+
+            boolean addResult = bank.addAccount(acct);
+            int numAccounts1 = bank.totalAccounts();
+            // TODO run after fixing signature
+            // int findAcct1 = bank.find(acct.getAccountID());
+            int findAcct1 = 0;
+
+            writer.write(
+                String.format(
+                    "Bank init: getCount=%d, find=%d",
+                    numAccounts0, 
+                    findAcct0
+                ) + System.lineSeparator()
+            );
+
+            writer.write(
+                String.format(
+                    "After add: result=%b, getCount=%d, find=%d",
+                    addResult,
+                    numAccounts1, 
+                    findAcct1
+                ) + System.lineSeparator()
+            );
+
+            writer.close();
+        } catch (IOException e) { e.printStackTrace(); }
+
+    }
+}

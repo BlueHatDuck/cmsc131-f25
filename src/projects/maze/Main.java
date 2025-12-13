ec164065e25b811fe0577c7abf0f0cd6f0524dcd
John M Dusel <jdusel@frederick.edu>
2025-12-13 11:52:08 +0000

    FBK: Maze Phase 2

--- src/projects/maze/Main.java
+++ src/projects/maze/Main.java
@@ -8,8 +8,24 @@ public class Main {
         maze.serialize("data/sample_maze_phase1.txt");
     }
 
+    private static void checkSolve(String filename, String descr) {
+        Maze maze = MazeReader.load(filename);
+        if (maze.solve()) {
+            System.out.println("Solved " + descr);
+            maze.serialize("data/checkSolve.txt");
+        } else {
+            System.out.println("No solution to " + descr);
+        }
+    }
+
+    static void phase2() {
+        checkSolve("data/hard_maze.txt", "hard maze");
+        checkSolve("data/hard_maze_nosol.txt", "hard maze (nosol)");
+    }
+
     public static void main(String[] args) {
         phase1();
+        phase2();
     }
 
 }
